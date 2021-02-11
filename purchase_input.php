425f82550f69709b3dfe67a20c9e7e1f3d1b7616
ch-study-account <ch.harada008@gmail.com>
2021-02-12 00:39:26 +0900

    購入画面を実装

--- purchase_input.php
+++ purchase_input.php
@@ -13,7 +13,7 @@
 	<?php require 'menu.php'; ?>
 	<?php
 	//ログインの有無の確認
-	if(!isset($_POST['customer'])){
+	if(!isset($_SESSION['customer'])){
 		echo "購入手続きを行うにはログインしてください。";
 	}else if(empty($_SESSION['product'])){
 		echo "カートに商品がありません。";
@@ -21,9 +21,15 @@
 		//正常処理
 	?>
 	<p>お名前：<?= $_SESSION['customer']['name'] ?></p>
-	<p>お名前：<?= $_SESSION['customer']['name'] ?></p>
-	}
-	
+	<p>ご住所：<?= $_SESSION['customer']['address'] ?></p>
+	<hr>
+	<?php require 'cart.php'; ?>
+	<hr>
+	<p>内容をご確認いただき、購入を確定してください。</p>
+	<a href="purchase_output.php">購入を確定する。</a>
+	<?php
+		}
+	?>
 </body>
 
 </html>
