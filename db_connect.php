be4fbfceadca98ae25ebacf4426304dd9d75ad9b
ch-study-account <74999962+ch-study-account@users.noreply.github.com>
2021-06-08 11:11:57 +0900

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,19 @@
 <?php
   // データベースユーザ
-  $user = 'root';
-  $password = '';
+  //$user = 'root';
+  $user = 'foaexeereyhmch';
+  //$password = '';
+  $password = '52ba0ee1be8c68da37638cbb6b1876c5da70f3a45505675c8a90b8b17435c93c';
   // 利用するデータベース
-  $dbName = 'shop';
+  //$dbName = 'shop';
+  $dbName = 'da5asa8fcdmdo';
   // MySQLサーバ
-  $host = 'localhost';
+  //$host = 'localhost';
+  $host = 'ec2-54-243-92-68.compute-1.amazonaws.com';
+  $port ='5432'
   // MySQLのDSN文字列
-  $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+  //$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+  $dsn = "pgsql:host={$host};port='5432';dbname={$dbName};charset=utf8";
   //MySQLデータベースに接続する
   try {
     $pdo = new PDO($dsn, $user, $password);
